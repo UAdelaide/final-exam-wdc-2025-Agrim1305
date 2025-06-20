cf3aac9890f938599f208f1632651fac6e42b217
Agrim22 <137141730+Agrim22@users.noreply.github.com>
2025-06-20 12:51:38 +0000

    [automatic commit] 2025-06-20 12:51:38 UTC

--- part1/app.js
+++ part1/app.js
@@ -3,11 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var searchesRouter = require('./routes/searches.js');
+
 var app = express();
 
 // view engine setup
@@ -22,6 +21,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api',searchesRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
