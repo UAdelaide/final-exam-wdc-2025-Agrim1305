170e0a41d3f9dcb62fbe23799422fbb67bdb8b7e
Agrim22 <137141730+Agrim22@users.noreply.github.com>
2025-06-20 14:12:09 +0000

    [automatic commit] 2025-06-20 14:12:05 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models/db');
+const db = require('../models/db.js');
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
@@ -37,22 +37,50 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
-    `, [email, password]);
+      WHERE username = ? AND password_hash = ?
+    `, [username, password]);
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    res.json({ message: 'Login successful', user: rows[0] });
+    req.session.user = {
+      user_id: rows[0].user_id,
+      username: rows[0].username,
+      role: rows[0].role
+    };
+
+    res.json({ message: 'Login successful', role: rows[0].role });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
 });
 
+// POST /api/users/logout
+router.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
+
+    res.clearCookie('connect.sid');
+    res.json({ message: 'Logged out' });
+  });
+});
+
+// Get all dogs for a specific owner
+router.get('/:id/dogs', async (req, res) => {
+  const ownerId = req.params.id;
+  try {
+    const [rows] = await db.query('SELECT dog_id, name, size FROM Dogs WHERE owner_id = ?', [ownerId]);
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to load dogs' });
+  }
+});
 module.exports = router;
