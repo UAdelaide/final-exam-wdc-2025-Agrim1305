138f959d85d40ae6af7ea31a9a903ed8bc519675
Agrim22 <137141730+Agrim22@users.noreply.github.com>
2025-06-21 04:14:01 +0000

    [automatic commit] 2025-06-21 04:14:01 UTC

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(session({
-    secret: 'mydogwalkapp2025$@#*!',
+    secret: 'mydogwalkerapp@123!#$',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
